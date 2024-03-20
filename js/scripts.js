58d71b757134b4d41995882872056333d5a1e12a
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-03-20 15:07:41 -0400

    pockemonList
    Array with 4 Pockemons and their characteristics were added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello World');
-var favouriteFood='Steak';
-document.write(favouriteFood);
+let pockemonList=[
+    {name:'bulbasaur', height:0.7, weight:6.9, type:['grass','poison']},
+    {name:'charmander', height:0.6, weight:8.5, type:'fire'},
+    {name:'cubone', height:0.4, weight:6.5, type:'ground'},
+    {name:'squirtle', height:0.5, weight:9.0, type:'water'}
+];
