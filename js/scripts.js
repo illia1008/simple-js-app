0075c0a1023ca8271bfcd776180fd9293d147746
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-03-21 23:31:10 -0400

    Big Pokemon determination
    Big Pokemon determination code was added to the script.js file. Also, font and background color were added to css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,42 @@
-let pockemonList=[
-    {name:'bulbasaur', height:0.7, weight:6.9, type:['grass','poison']},
-    {name:'charmander', height:0.6, weight:8.5, type:'fire'},
-    {name:'cubone', height:0.4, weight:6.5, type:'ground'},
-    {name:'squirtle', height:0.5, weight:9.0, type:'water'}
+// We create Pokemon array with their characteristics
+let pockemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        weight: 6.9,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        weight: 8.5,
+        type: 'fire'
+    },
+    {
+        name: 'Cubone',
+        height: 0.4,
+        weight: 6.5,
+        type: 'ground'
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        weight: 9.0,
+        type: 'water'
+    }
 ];
+
+// This is conditional criteria
+let heightLimit = 0.65;
+
+// Create loop to check every element of the array
+for (let i = 0; i < pockemonList.length; i++) {
+    if (pockemonList[i].height > heightLimit) {
+        document.write(pockemonList[i].name + ' (height:' + pockemonList[i].height +
+            ');' + ' - Wow, that\'s big!' + '<br>');
+    }
+    else {
+        document.write(pockemonList[i].name + ' (height:' + pockemonList[i].height +
+            ');' + '<br>');
+    }
+}
