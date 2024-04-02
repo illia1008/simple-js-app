508aa2502bdabd06c0085082193abf651c1b7048
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-04-02 14:11:38 -0400

    IIFE
    The array was wrapped in an IIFE and the code was modified to make forEach loop work

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,66 @@
-// We create Pokemon array with their characteristics
-let pockemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        weight: 6.9,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        weight: 8.5,
-        type: 'fire'
-    },
-    {
-        name: 'Cubone',
-        height: 0.4,
-        weight: 6.5,
-        type: 'ground'
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        weight: 9.0,
-        type: 'water'
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            weight: 6.9,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            weight: 8.5,
+            type: 'fire'
+        },
+        {
+            name: 'Cubone',
+            height: 0.4,
+            weight: 6.5,
+            type: 'ground'
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            weight: 9.0,
+            type: 'water'
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.add(
+    {
+        name: 'Eevee',
+        height: 0.3,
+        weight: 6.5,
+        type: ['normal']
+    })
+
+console.log(pokemonRepository.getAll());
+
+let pokemonList = pokemonRepository.getAll();
 
 // This is conditional criteria
 let heightLimit = 0.65;
 
-// Create loop to check every element of the array
-// for (let i = 0; i < pockemonList.length; i++) {
-//     if (pockemonList[i].height > heightLimit) {
-//         document.write(pockemonList[i].name + ' (height:' + pockemonList[i].height +
-//             ');' + ' - Wow, that\'s big!' + '<br>');
-//     }
-//     else {
-//         document.write(pockemonList[i].name + ' (height:' + pockemonList[i].height +
-//             ');' + '<br>');
-//     }
-//
-
 // forEach loop method1
-pockemonList.forEach(pockemon);
+pokemonList.forEach(pokemon);
 
-function pockemon(element) {
+function pokemon(element) {
     if (element.height > heightLimit) {
         document.write(element.name + ' (height:' + element.height +
             ');' + ' - Wow, that\'s big!' + '<br>');
@@ -56,7 +72,7 @@ function pockemon(element) {
 }
 
 // forEach loop method2
-// pockemonList.forEach(function(element) {
+// pokemonList.forEach(function(element) {
 //     if (element.height > heightLimit) {
 //         document.write(element.name + ' (height:' + element.height +
 //             ');' + ' - Wow, that\'s big!' + '<br>');
@@ -68,7 +84,7 @@ function pockemon(element) {
 // });
 
 // forEach loop method3
-// pockemonList.forEach(element => {
+// pokemonList.forEach(element => {
 //     if (element.height > heightLimit) {
 //         document.write(element.name + ' (height:' + element.height +
 //             ');' + ' - Wow, that\'s big!' + '<br>');
