bcdb25508df4365ecc00eb157c978ff5af4233c5
illia1008 <158865982+illia1008@users.noreply.github.com>
2024-04-10 19:00:22 -0400

    Fetched data
    console.log of the fetched data

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=4';
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         if (
@@ -44,6 +44,7 @@ let pokemonRepository = (function () {
               detailsUrl: item.url
             };
             add(pokemon);
+            console.log ( pokemon );
           });
         }).catch(function (e) {
           console.error(e);
